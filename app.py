b3f96800dc397cb174eeb51f00af1e685e5d941a
YiYang <yangyi040609@gmail.com>
2026-03-27 02:46:29 +0100

    chore: initialize minimal Flask app

---
+++ app.py
@@ -0,0 +1,18 @@
+﻿from flask import Flask
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+
+    @app.get('/health')
+    def health_check():
+        return {'status': 'ok'}, 200
+
+    return app
+
+
+app = create_app()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
